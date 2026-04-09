d37d38abcf0c8dc5b8be824b97b41c3ea4eeaea0
Edward <vwedward@mail.ru>
2026-04-09 18:22:52 +0300

    + experiment

---
+++ src/ru/edward/experiment.java
@@ -0,0 +1,24 @@
+/*
+ * ©Edward, 2026
+ */
+
+/*
+ * ©Edward, 2026
+ */
+
+/*
+ * ©Edward, 2026
+ */
+
+package ru.edward;
+
+import java.util.Arrays;
+
+public class experiment {
+    public void main(String[] param) {
+        String myString = "1,2,3,4";
+        String[] arr = myString.split(",");
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
